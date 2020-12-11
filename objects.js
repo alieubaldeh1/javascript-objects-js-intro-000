db280218500e4d1867b0c0bdaa117f9ba7824f36
Alieu Baldeh <alieubaldeh@gmail.com>
2020-12-11 19:35:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Jay : "NYStateOfMind" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist = {artistName : songTitle}
+  playlist[artistName] = songTitle
   return playlist
 }
